2fb04240b5b25b421f9436b92efc63762c7984b4
visnz <visn0518@gmail.com>
2019-08-16 15:36:36 +0800

    创建js优化，增加注释

--- js/moejq.js
+++ js/moejq.js
@@ -1,6 +1,9 @@
+"use strict";
+// 相关博客1：https://visn.online/post/daily6/moescript/
+// 相关博客2：https://visn.online/post/daily6/moejqscript/
 
 // ------------------------ //
-//   常量区
+//   1 常量区
 // ------------------------ //
 // 默认大部分事件为60秒执行一次,快速事件为15秒,慢速事件为5分钟
 const defaultDelay = 60000
@@ -15,51 +18,51 @@ const AllClassName = "moescript"
 // 
 // div.craft:nth-child(1) > div:nth-child(6) > div:nth-child(1)  
 // 6表示定位到"全部" 上面这行选择触发全部转换
-const 合成资源定位表 = {
+const SourcePositionMap = {
     "木材": 1,
-    "": 2,
+    // "": 2,
     "木梁": 3,
     "石板": 4,
     "金属板": 5,
     "钢": 6,
-    "": 7,
+    // "": 7,
     "齿轮": 8,
-    "": 9,
-    "": 10,
+    // "": 9,
+    // "": 10,
     "脚手架": 11,
     "船": 12,
-    "": 13,
-    "": 14,
+    // "": 13,
+    // "": 14,
     "羊皮纸": 15,
     "手稿": 16,
     "概要": 17,
     "蓝图": 18,
-    "": 19,
+    // "": 19,
     "巨石": 20,
 }
-const 职业={
-    "伐木工":0,
-    "农民":1,
-    "学者":2,
-    "猎人":3,
-    "矿工":4,
-    "牧师":5,
-    "地质学家":6,
-    "工程师":7,
+const Jobs = {
+    "伐木工": 0,
+    "农民": 1,
+    "学者": 2,
+    "猎人": 3,
+    "矿工": 4,
+    "牧师": 5,
+    "地质学家": 6,
+    "工程师": 7,
 }
 
 // ------------------------ //
-//   游戏操作接口
+//   2 游戏操作接口
 // ------------------------ //
 
-// 指定资源进行全部生产,可传入名称与编号,配合 合成资源定位表
+// 指定资源进行全部生产,可传入名称与编号,配合 SourcePositionMap
 // GenerateResourceAll(20,1) 巨石换算1单位(未算加成)
 // GenerateResourceAll("蓝图",2) 蓝图换算25单位(未算加成)
 // GenerateResourceAll("蓝图",3) 蓝图换算100单位(未算加成)
 // GenerateResourceAll(1) 换算全部木材
 function GenerateResourceAll(target, count = 4) {
     var sel = (count < 1 ? 1 : count > 4 ? 4 : count) + 2
-    i = (typeof target === 'string' ? 合成资源定位表[target] : target)
+    i = (typeof target === 'string' ? SourcePositionMap[target] : target)
     $("div.craft:nth-child(" + i + ") > div:nth-child(" + sel + ") > div:nth-child(1)").click()
 }
 
@@ -69,16 +72,16 @@ function GenerateResourceAll(target, count = 4) {
 // jobIncrease("伐木工",3) => +25
 // jobIncrease("伐木工",4) => +all
 // jobIncrease("伐木工",1,false) => -1
-function jobIncrease(job,count=1,increase=true){
-    var sel =( (count < 1 ? 1 : count > 4 ? 4 : count) + 2)%4+1
-    i = (typeof job === 'string' ? 职业[job] : job)
-    index=i*8+(increase?4:0)+sel-1
+function jobIncrease(job, count = 1, increase = true) {
+    var sel = ((count < 1 ? 1 : count > 4 ? 4 : count) + 2) % 4 + 1
+    i = (typeof job === 'string' ? Jobs[job] : job)
+    index = i * 8 + (increase ? 4 : 0) + sel - 1
     $("div.panelContainer .modern a")[index].click()
 }
 
 
 // ------------------------ //
-//   脚本逻辑区
+//   3 脚本逻辑区
 //      _开头为中间操作
 // ------------------------ //
 
@@ -114,21 +117,21 @@ function jobIncrease(job,count=1,increase=true){
 赞美太阳 = () => { $("#fastPraiseContainer > a:nth-child(1)")[0].click() }
 _进入篝火面板 = () => { $("a.tab:nth-child(1)")[0].click() }
 _进入管理面板 = () => { $("a.tab:nth-child(3)")[0].click() }
-_管理职业 = () => { $("div.panelContainer:nth-child(3) > div:nth-child(3) > table:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > div:nth-child(3) > div:nth-child(1) > span:nth-child(1)").click() }
+_管理Jobs = () => { $("div.panelContainer:nth-child(3) > div:nth-child(3) > table:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > div:nth-child(3) > div:nth-child(1) > span:nth-child(1)").click() }
 _提拔小猫 = () => { $("div.panelContainer:nth-child(3) > div:nth-child(3) > table:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > div:nth-child(4) > div:nth-child(1)").click() }
 var preIndex = 0
 _记录当前面板Index = () => { preIndex = $(".activeTab").parent().children(".activeTab").index() }
 _返回记录面板Index = () => { $("a.tab:nth-child(" + (preIndex + 1) + ")")[0].click() }
-_检查粮食增量是否为正=()=>{
-    return $("#leftColumnViewport > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4)").text().split("+").length==2
+_检查粮食增量是否为正 = () => {
+    return $("#leftColumnViewport > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4)").text().split("+").length == 2
 }
-_保存游戏=()=>{
+_保存游戏 = () => {
     $("div.links-block:nth-child(6) > a:nth-child(1)").click()
 }
-管理职业 = () => {
+管理Jobs = () => {
     _记录当前面板Index()
     _进入管理面板()
-    _管理职业()
+    _管理Jobs()
     _返回记录面板Index()
 }
 小猫升职器 = () => {
@@ -137,14 +140,14 @@ _保存游戏=()=>{
     _提拔小猫()
     _返回记录面板Index()
 }
-var pao=0
+var pao = 0
 高速卡机刨木法 = () => {
     _记录当前面板Index()
     _进入篝火面板()
-    if(pao>=12){
+    if (pao >= 6) {
         _保存游戏()
         logThis("游戏已保存")
-        pao=0
+        pao = 0
     }
     pao++
     for (i = 0; i < 36000; i++) { $(".bldGroupContainer .modern")[0].click() }
@@ -153,10 +156,10 @@ var pao=0
 中速卡机刨木法 = () => {
     _记录当前面板Index()
     _进入篝火面板()
-    if(pao>=60){
+    if (pao >= 30) {
         _保存游戏()
         logThis("游戏已保存")
-        pao=0
+        pao = 0
     }
     pao++
     for (i = 0; i < 12000; i++) { $(".bldGroupContainer .modern")[0].click() }
@@ -165,10 +168,10 @@ var pao=0
 低速卡机刨木法 = () => {
     _记录当前面板Index()
     _进入篝火面板()
-    if(pao>=120){
+    if (pao >= 60) {
         _保存游戏()
         logThis("游戏已保存")
-        pao=0
+        pao = 0
     }
     pao++
     for (i = 0; i < 6000; i++) { $(".bldGroupContainer .modern")[0].click() }
@@ -188,20 +191,25 @@ var pao=0
 合成蓝图 = () => { GenerateResourceAll("蓝图") }
 合成巨石 = () => { GenerateResourceAll("巨石") }
 
-// list 数据结构:
+// model 数据结构:
 //  DisplayName 将会作为id在按钮上出现
 //  如果没有指定scriptFunc,则默认调用同名函数
 //  如果没有指定timer,则默认使用defaultDelay常量
-所有操作 = [
+AllOperators = [
     {
         panel: "基本",
         list: [
             { DisplayName: "抬头看天空", timer: fastDelay },
             { DisplayName: "自动打猎" },
             { DisplayName: "赞美太阳" },
-            { DisplayName: "管理职业", timer: verySlowDelay },
+            { DisplayName: "管理Jobs", timer: verySlowDelay },
             { DisplayName: "小猫升职器", timer: verySlowDelay },
         ]
+        //     }, {
+        //         panel: "策略",
+        //         list: [
+        //             // { DisplayName: "自动最低农民" },
+        //         ]
     }, {
         panel: "策略",
         list: [
@@ -235,39 +243,39 @@ var pao=0
 ]
 
 // ------------------------ //
-//   UI逻辑
+//   4 UI逻辑
 // ------------------------ //
 
 // 在画面写入组件
 $("#rightColumn").prepend(
     '<div>' +
-        '<div style="display:flex;flex-wrap:wrap">' +
-            所有操作.map((i) => {
-                return '<input id="' + i.panel + '按钮" type="button" style="display:inline" value="' + i.panel + '">'
-            }).reduce((s, c) => s = s + c) +
-            '<input id="全自动关" type="button"  style="display:inline" value="全部关闭">' +
-            '<span><input id="立马执行" type="checkbox"  style="display:inline" checked><span>立马执行</span></span>' +
-        '</div>' +
-        所有操作.map((j) => {
-            return '<div id="' + j.panel + '" style="display:none";>' +
-                '<div style="display:flex;flex-wrap:wrap">' +
-                j.list.map((e) =>
-                    '<span style="white-space:nowrap;"><input class="' + AllClassName +
-                    '" id="' + e.DisplayName +
-                    '" type="checkbox" style="display:inline"><span>' + e.DisplayName +
-                    '</span></span>'
-                ).reduce((s, c) => s = s + c) +
-                '</div>' +
-                '</div>'
-        }).reduce((s, c) => s = s + c) +
-        '<div>' +
-            '<div id="' + AllClassName + 'log"style="min-height:300px;max-height:300px;overflow:auto;"></div>' +
-        '</div>' +
+    '<div style="display:flex;flex-wrap:wrap">' +
+    AllOperators.map((i) => {
+        return '<input id="' + i.panel + '按钮" type="button" style="display:inline" value="' + i.panel + '">'
+    }).reduce((s, c) => s = s + c) +
+    '<input id="全自动关" type="button"  style="display:inline" value="全部关闭">' +
+    '<span><input id="立马执行" type="checkbox"  style="display:inline" checked><span>立马执行</span></span>' +
+    '</div>' +
+    AllOperators.map((j) => {
+        return '<div id="' + j.panel + '" style="display:none";>' +
+            '<div style="display:flex;flex-wrap:wrap">' +
+            j.list.map((e) =>
+                '<span style="white-space:nowrap;"><input class="' + AllClassName +
+                '" id="' + e.DisplayName +
+                '" type="checkbox" style="display:inline"><span>' + e.DisplayName +
+                '</span></span>'
+            ).reduce((s, c) => s = s + c) +
+            '</div>' +
+            '</div>'
+    }).reduce((s, c) => s = s + c) +
+    '<div>' +
+    '<div id="' + AllClassName + 'log"style="min-height:300px;max-height:300px;overflow:auto;"></div>' +
+    '</div>' +
     '</div>'
 )
 
 // 在组件上添加事件
-所有操作.map((k) => {
+AllOperators.map((k) => {
     k.list.map((i) => {
         if (i.timer == null) i.timer = defaultDelay;
         if (i.scriptFunc == null) i.scriptFunc = (() => eval(i.DisplayName + "()"));
@@ -278,7 +286,7 @@ $("#rightColumn").prepend(
                     i.scriptFunc()
                     logThis('[__自动__] ' + i.DisplayName)
                 }, i.timer)
-                if($("#立马执行")[0].checked){
+                if ($("#立马执行")[0].checked) {
                     i.scriptFunc()
                     logThis('[____立马] ' + i.DisplayName + "(ง •̀_•́)ง")
                 }
@@ -291,7 +299,7 @@ $("#rightColumn").prepend(
 })
 
 // 为面板设置开关基本操作
-所有操作.map((k) => {
+AllOperators.map((k) => {
     $("#" + k.panel + "按钮").click((e) => {
         $("#" + k.panel).attr("style", "display:" + (($("#" + k.panel).attr("style") == "display:block") ? "none" : "block"))
     })
@@ -299,14 +307,14 @@ $("#rightColumn").prepend(
 
 // 全自动关的功能
 $("#全自动关").click((e) => {
-    所有操作.map((j) => j.list.map((k) => clearInterval(k.AutoID)))
+    AllOperators.map((j) => j.list.map((k) => clearInterval(k.AutoID)))
     $("." + AllClassName).each((i, e) => { $("#" + e.id)[0].checked = false });
     logThis("清除了所有脚本")
 })
 
 // 停止某个定时器
 function stop(displayname) {
-    所有操作.map((j) => j.list.map((k) => {
+    AllOperators.map((j) => j.list.map((k) => {
         if (k.DisplayName == displayname) {
             clearInterval(k.AutoID)
             $("#" + k.DisplayName)[0].checked = false;
